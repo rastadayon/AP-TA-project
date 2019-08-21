b4f743147e750642e5295bba2851c03e1fe422f8
rastadayon <rasta.tadayon1378@gmail.com>
2019-08-21 12:21:01 +0430

    kids.hpp

---
+++ kids.hpp
@@ -0,0 +1,27 @@
+#ifndef __KIDS_H__
+#define __KIDS_H__
+
+#include <iostream>
+#include <vector>
+
+class kid
+{
+private:
+    float velocity;
+    float radius;
+    
+public:
+    kid(/* args */);
+    ~kid();
+};
+
+kid::kid(/* args */)
+{
+}
+
+kid::~kid()
+{
+}
+
+
+#endif
